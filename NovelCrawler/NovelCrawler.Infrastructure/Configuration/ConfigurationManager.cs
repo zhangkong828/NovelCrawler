1a3c2d631e8f55a0ec2371f7227bff82f0e0af1b
ZhangKong <602488225@qq.com>
2018-08-16 20:13:01 +0800

    ConfigurationManager

---
+++ NovelCrawler/NovelCrawler.Infrastructure/Configuration/ConfigurationManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NovelCrawler.Infrastructure.Configuration
+{
+    public class ConfigurationManager
+    {
+        static IConfiguration Configuration;
+        static ConfigurationManager()
+        {
+            var provider = new EnvironmentVariablesConfigurationProvider();
+            provider.Load();
+            provider.TryGet("ASPNETCORE_ENVIRONMENT", out string environmentName);
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// 获取配置节点，使用规则详情：https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1&tabs=basicconfiguration
+        /// </summary>
+        /// <param name="key">不区分大小写</param>
+        /// <returns></returns>
+        public static string GetValue(string key)
+        {
+            return Configuration[key];
+        }
+
+        public static T GetValue<T>(string key)
+        {
+            return Configuration.GetValue<T>(key);
+        }
+    }
+}
---
+++ NovelCrawler/NovelCrawler.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NovelCrawler.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
